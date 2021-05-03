7233ecc6546989b9947821894d43e68a8c3ad9cb
VascoII <fabriceds@yahoo.fr>
2021-05-03 19:11:54 +0200

    Update: provider added

--- coinWallet/app/Providers/AppServiceProvider.php
+++ coinWallet/app/Providers/AppServiceProvider.php
@@ -2,18 +2,23 @@
 
 namespace App\Providers;
 
-use App\Domain\Repositories\Coinbase\TransactionRepository as TransactionRepositoryInterface;
+use App\Domain\Repositories\Transverse\TransactionRepository as TransactionRepositoryInterface;
 use App\Domain\Repositories\CoinMarketCap\QuoteRepository as QuoteRepositoryInterface;
-use App\Domain\Repositories\Coinbase\CoinRepository as CoinRepositoryInterface;
+use App\Domain\Repositories\Transverse\CoinRepository as CoinRepositoryInterface;
 use App\Domain\Services\Coinbase\GetAlertInfoService as GetAlertInfoServiceInterface;
 use App\Domain\Services\CoinMarketCap\QuoteLatestService as QuoteLatestServiceInterface;
 use App\Domain\Services\MetaDataService as MetaDataServiceInterface;
-use App\Infrastructure\Repositories\Coinbase\CoinRepository;
-use App\Infrastructure\Repositories\Coinbase\TransactionRepository;
+use App\Domain\Services\Coinbase\GetBalanceService as CoinbaseGetBalanceServiceInterface;
+use App\Domain\Services\Binance\GetBalanceService as BinanceGetBalanceServiceInterface;
+use App\Infrastructure\Repositories\Transverse\CoinRepository;
+use App\Infrastructure\Repositories\Transverse\TransactionRepository;
 use App\Infrastructure\Repositories\CoinMarketCap\QuoteRepository;
 use App\Infrastructure\Service\Coinbase\GetAlertInfoService;
+use App\Infrastructure\Service\Coinbase\GetBalanceService as CoinbaseGetBalanceService;
+use App\Infrastructure\Service\Binance\GetBalanceService as BinanceGetBalanceService;
 use App\Infrastructure\Service\CoinMarketCap\MetaDataCoinMarketCapService;
 use App\Infrastructure\Service\CoinMarketCap\QuoteLatestService;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -34,6 +39,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(MetaDataServiceInterface::class, MetaDataCoinMarketCapService::class);
         $this->app->bind(QuoteLatestServiceInterface::class, QuoteLatestService::class);
         $this->app->bind(GetAlertInfoServiceInterface::class, GetAlertInfoService::class);
+        $this->app->bind(CoinbaseGetBalanceServiceInterface::class, CoinbaseGetBalanceService::class);
+        $this->app->bind(BinanceGetBalanceServiceInterface::class, BinanceGetBalanceService::class);
     }
 
     /**
